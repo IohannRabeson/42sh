1e43f0b49975fb533d96b1d69d1ea3da84d177f5
irabeson <irabeson@student.42.fr>
2014-02-09 22:02:54 +0100

    Norme

--- libft/src/ft_str_buf_clone.c
+++ libft/src/ft_str_buf_clone.c
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/09 15:38:44 by irabeson          #+#    #+#             */
-/*   Updated: 2014/02/09 15:42:32 by irabeson         ###   ########.fr       */
+/*   Updated: 2014/02/09 21:35:41 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@
 void	str_buf_clone(t_str_buf *clone, t_str_buf const *strbuf)
 {
 	str_buf_clear(clone);
-	str_buf_app_strn(clone, strbuf->buffer, strbuf->size);	
+	str_buf_app_strn(clone, strbuf->buffer, strbuf->size);
 }
